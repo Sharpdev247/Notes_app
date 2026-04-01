8878d3ed651facedf3062d341f82073de5e22ef7
Sharpdev247 <sharpdev247@gmail.com>
2026-04-02 01:06:46 +0500

    Change package name for Google Play compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.notes_app"
+    namespace = "com.sharpdev.notesapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.notes_app"
+        applicationId = "com.basitkhan.notesapp"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
